60e442191a46de89ad3c9f58be61f08626d4aed2
James-Do9 <dojames3@yahoo.com>
2021-01-05 19:50:09 +0000

    Update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,29 +1,64 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			taskList: [],
+			task: ""
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			loadToDoListData: () => {
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/dojam")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(jsonifiedResponse => setStore({ taskList: jsonifiedResponse }))
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
 			},
+
+			addList: task => {
+				let store = getStore();
+				if (task) {
+					var updatedList = store.taskList.concat({ label: task, done: false });
+					setStore({ taskList: updatedList });
+					fetch("https://assets.breatheco.de/apis/fake/todos/user/dojam", {
+						method: "PUT", // or 'POST'
+						body: JSON.stringify(updatedList), // data can be `string` or {object}!
+						headers: {
+							"Content-Type": "application/json"
+						}
+					})
+						.then(res => res.json())
+						.then(response => console.log("Success:", JSON.stringify(response)))
+						.catch(error => console.error("Error:", error));
+				}
+			},
+
+			removeList: index => {
+				let store = getStore();
+				let updatedTaskList = store.taskList;
+				updatedTaskList.splice(index, 1);
+				setStore({ taskList: updatedTaskList });
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/dojam", {
+					method: "PUT", // or 'POST'
+					body: JSON.stringify(updatedTaskList), // data can be `string` or {object}!
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(res => res.json())
+					.then(response => console.log("Success:", JSON.stringify(response)))
+					.catch(error => console.error("Error:", error));
+			},
+
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,44 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [task, setTask] = useState("");
+	return (
+		<div>
+			<h1>To Do List:</h1>
+			<div className="ToDoList">
+				<div className="list-group list-group-flush">
+					<input
+						className="list-group-item"
+						onChange={e => setTask(e.target.value)}
+						value={task}
+						type="text"
+						onKeyUp={e => {
+							if (e.keyCode == 13) {
+								actions.addList(task);
+								setTask("");
+							}
+						}}
+						placeholder="What needs to be done?"
+					/>
+				</div>
+			</div>
+			<div className="ToDoList paper">
+				<ol className="list-group list-group-flush">
+					{store.taskList.map((content, index) => {
+						return (
+							<li className="list-group-item listText" key={index}>
+								{content.label}{" "}
+								<span className="float-right" onClick={() => actions.removeList(index)}>
+									<i className="fas fa-times" style={{ color: "red " }} />
+								</span>
+							</li>
+						);
+					})}
+				</ol>
+			</div>
+		</div>
+	);
+};
